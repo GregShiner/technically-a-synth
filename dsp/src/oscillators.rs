6ddfd78e23f4d20c56cbc91aa8cc3b86619e0cae
Greg Shiner <greg@gregshiner.com>
2026-06-29 23:30:51 -0400

    Reorganize dsp library

--- dsp/src/oscillators.rs
+++ dsp/src/oscillators.rs
@@ -1,7 +1,10 @@
 //! Grossly, incomprehensibly generic oscillators
 use core::f32::consts::PI;
 
-use dasp::{Frame, Signal};
+use dasp::{
+    Frame, Signal,
+    signal::bus::{Bus, Output, SignalBus},
+};
 use libm::sinf;
 
 pub struct ConstHz {
@@ -132,4 +135,44 @@ impl<P: Signal<Frame = f32>> Signal for Triangle<f32, P> {
         let phase = self.phase.next();
         (4.0 * (phase - 0.5).abs()) - 1.0
     }
+}
+
+pub struct Oscillator<F: Frame, S: Signal<Frame = F>> {
+    pub bus: Bus<S>,
+    pub main_send: Output<S>,
+}
+
+impl<F: Frame, S: Signal<Frame = F>> Signal for Oscillator<F, S> {
+    type Frame = F;
+
+    fn next(&mut self) -> Self::Frame {
+        self.main_send.next()
+    }
+}
+
+impl<F: Frame, S: Signal<Frame = F>> From<S> for Oscillator<F, S> {
+    fn from(signal: S) -> Self {
+        let bus = signal.bus();
+        let main_send = bus.send();
+        Self { bus, main_send }
+    }
+}
+
+pub fn square_oscillator(freq: f32, sample_rate: f32) -> Square<f32, LinearPhase<f32, ConstHz>> {
+    Square::from_phase(LinearPhase::from_freq(ConstHz::new(freq), sample_rate))
+}
+
+pub fn sine_oscillator(freq: f32, sample_rate: f32) -> Sine<f32, LinearPhase<f32, ConstHz>> {
+    Sine::from_phase(LinearPhase::from_freq(ConstHz::new(freq), sample_rate))
+}
+
+pub fn saw_oscillator(freq: f32, sample_rate: f32) -> Saw<f32, LinearPhase<f32, ConstHz>> {
+    Saw::from_phase(LinearPhase::from_freq(ConstHz::new(freq), sample_rate))
+}
+
+pub fn triangle_oscillator(
+    freq: f32,
+    sample_rate: f32,
+) -> Triangle<f32, LinearPhase<f32, ConstHz>> {
+    Triangle::from_phase(LinearPhase::from_freq(ConstHz::new(freq), sample_rate))
 }
