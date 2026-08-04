b235387e6c3cab6e59684a6d46b02788ea6faabb
Greg Shiner <greg@gregshiner.com>
2026-08-03 20:27:19 -0400

    Change nodes to no longer be in place sorted

--- dsp/src/graph.rs
+++ dsp/src/graph.rs
@@ -19,35 +19,49 @@ enum NodeType {
 #[derive(Clone)]
 struct Node {
     node_type: NodeType,
-    input_nodes: Vec<usize, MAX_NODE_INPUTS>, // Indices of input nodes
-    assigned_input_buffers: Vec<usize, MAX_NODE_INPUTS>, // Indices of input puffers
-    assigned_output_buffers: Vec<usize, MAX_NODE_OUTPUTS>, // Indices of output puffers
-    assigned_scratch_buffers: Vec<usize, MAX_NODE_SCRATCH_BUFFERS>, // Indices of scratch puffers
+    /// Indices of input nodes
+    input_nodes: Vec<usize, MAX_NODE_INPUTS>,
+    /// Indices of input puffers
+    assigned_input_buffers: Vec<usize, MAX_NODE_INPUTS>,
+    /// Indices of output puffers
+    assigned_output_buffers: Vec<usize, MAX_NODE_OUTPUTS>,
+    /// Indices of scratch puffers
+    assigned_scratch_buffers: Vec<usize, MAX_NODE_SCRATCH_BUFFERS>,
 }
 
+// NOTE: In a previous iteration of this implementation, rather than updating and storing the
+// process order as a separate vector of indices, it would instead in place mutate the nodes
+// vector. I decided to change this back to a simpler approach that did not mutate the nodes, but
+// rather simply stored the array of indices in sorted order. My original reason for in place
+// mutating the array was to improve cache locality and simplicity of the process function. Storing
+// the nodes in the order in which they would be executed made sense, but already the size of a node
+// has grown to 120 bytes which means you won't be fitting more than 1 in a cache line anyway.
+// This also reduced errors when it came to accessing the nodes. If something didn't have its
+// indices updated properly after a node moved, it could reference the wrong node.
+
 struct RoutingGraph<const N: usize> {
-    // TODO: Maybe find a reasonable way to precalculate and store the routing without needing to
-    // scan the entire edge_list every time.
+    /// Vector of nodes in graph
     nodes: Vec<Node, N>,
+    /// Order to execute the nodes in, stored as indices into the nodes vec
+    process_order: Vec<usize, N>,
+    /// Index of the buffer pool that contains the output for the right channel
     left_output_buffer: usize,
+    /// Index of the buffer pool that contains the output for the left channel
     right_output_buffer: usize,
 }
 
 impl<const N: usize> RoutingGraph<N> {
-    fn process(&mut self) -> () {
+    pub fn process(&mut self) {
         todo!()
     }
 
-    /// In place mutates the ordering of the nodes to be topologically sorted.
+    /// Updates the process_order so the nodes are executed in topological order
     /// Reassigns the input_nodes of the nodes after sorting so the connections stay in tact after
     /// reordering the array.
     /// Returns Err(()) if graph contains a cycle
     fn topological_sort(&mut self) -> Result<(), ()> {
-        // Stores copies of the Nodes in a sorted order
-        let mut sorted: Vec<Node, N> = Vec::new();
-        // Index of this array matches the index of the original array, the value in the index of
-        // that node in the new array. We need to keep track of this so we can remap all of the input indices.
-        let mut index_map: Vec<usize, N> = (0..self.nodes.len()).map(|_| 0).collect();
+        // Stores indices of the Nodes in a sorted order
+        let mut sorted: Vec<usize, N> = Vec::new();
 
         let mut to_check: Vec<usize, N> = self
             .nodes
@@ -75,13 +89,11 @@ impl<const N: usize> RoutingGraph<N> {
         }
 
         while let Some(from) = to_check.pop() {
-            let new_index = sorted.len();
-            index_map[from] = new_index;
             // SAFETY: Pusing to the vec can fail if its full
             // But it should never actually overflow since it has the same capacity as the nodes Vec
             // and each node should only be added once.
             debug_assert!(!sorted.is_full());
-            unsafe { sorted.push_unchecked(self.nodes[from].clone()) };
+            unsafe { sorted.push_unchecked(from) };
             let outgoing_connections: Vec<usize, N> =
                 edges.get(&from).unwrap().iter().copied().collect();
             for to in outgoing_connections {
@@ -104,13 +116,7 @@ impl<const N: usize> RoutingGraph<N> {
             return Err(());
         }
         debug_assert_eq!(self.nodes.len(), sorted.len());
-        //debug_assert_eq!(FnvIndexSet::from(self.nodes), FnvIndexSet::from(sorted));
-        for node in self.nodes.iter_mut() {
-            for input_node in node.input_nodes.iter_mut() {
-                *input_node = index_map[*input_node]
-            }
-        }
-        self.nodes = sorted;
+        self.process_order = sorted;
         Ok(())
     }
 }
